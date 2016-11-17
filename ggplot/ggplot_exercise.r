4e3f12470a92556668e01043717178f382ec27dd
Caitlyn Wilhelm <Caitlyn Wilhelm>
2016-11-17 15:03:00 -0800

    finished exercise

--- ggplot/ggplot_exercise.r
+++ ggplot/ggplot_exercise.r
@@ -1,4 +1,5 @@
 library(ggplot2)
+install.packages('data.table')
 library(data.table)
 library(reshape2)
 library(dplyr)
@@ -9,14 +10,19 @@ load("suicides.rdata")
 suicides$age <- as.factor(suicides$age)
 
 all_suicides <- copy(suicides)
+
 suicides <- suicides %>% 
   group_by(year, age, sex) %>% 
   mutate(deaths = sum(deaths))
+
 
 #  Make a line plot of suicides by age
 # (year on the x axis, deaths on the y axis, different line for each age).
 # facet by sex.
 
+suicides_by_age <- ggplot(suicides,aes(x = year, y = deaths, color = age)) +
+                    geom_line() +
+                    facet_wrap(~sex,scales = "free")
 
 ##extra credit####
 
